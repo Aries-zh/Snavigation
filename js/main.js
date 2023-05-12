b5a2e2473e493d5ce5779a7bf0f9d5474ec98f3b
Aries <82352208+Aries-zh@users.noreply.github.com>
2023-05-12 22:52:49 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ window.addEventListener('load', function () {
     setTimeout(function () {
         iziToast.show({
             title: hello,
-            message: '欢迎来到 Snavigation'
+            message: '欢迎来到ACGN搜索页'
         });
     }, 800);
 
